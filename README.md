e2e22a378cab6cd88c1cd916c0261b3496552d13
Fabio Batista <fabio@sar.local>
2026-06-25 12:47:50 -0300

    SAR 2.0 - estrutura base inicial

---
+++ README.md
@@ -0,0 +1,12 @@
+# SAR
+
+Servidor de Automação Residencial
+
+## Instalação
+
+git clone https://github.com/fabiomuhl102/sar.git
+
+cd sar
+
+sudo bash install.sh
+
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "==================================="
+echo " SAR INSTALLER"
+echo "==================================="
+
+echo ""
+echo "Instalador em desenvolvimento"
+echo ""
+
+exit 0
